a8dffd73d33c9e8979b7a2c3ac0480ce9411b9ac
AndriiMaidanovych <ipz242_mav@student.ztu.edu.ua>
2026-01-07 03:09:56 +0200

    added migrations isDelForUser

---
+++ DormitoryManagementSystem/Migrations/20260107005525_AddIsDeletedToUser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DormitoryManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsDeletedToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Users",
+                type: "bit",
+                nullable: true);
+        }
+        /// <inheritdoc />
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Users");
+        }
+    }
+}
