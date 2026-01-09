8394385adafe3e6ba5a7d551cd7c07282b2bf04d
AndriiMaidanovych <ipz242_mav@student.ztu.edu.ua>
2026-01-09 11:44:45 +0200

    Added placeNumber to db

---
+++ DormitoryManagementSystem/Migrations/20260109091654_AddPlaceNumber.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DormitoryManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlaceNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PlaceNumber",
+                table: "RoomPlaces",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "RoomPlaces",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PlaceNumber",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "RoomPlaces",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "PlaceNumber",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "RoomPlaces",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "PlaceNumber",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "RoomPlaces",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "PlaceNumber",
+                value: 2);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PlaceNumber",
+                table: "RoomPlaces");
+        }
+    }
+}
