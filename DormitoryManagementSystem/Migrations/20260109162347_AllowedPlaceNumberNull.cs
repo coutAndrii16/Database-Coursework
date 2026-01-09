8cff283f8bef311c5f7eef7274c0c9034e7efef1
AndriiMaidanovych <ipz242_mav@student.ztu.edu.ua>
2026-01-09 18:25:30 +0200

    Allowed null migration

---
+++ DormitoryManagementSystem/Migrations/20260109162347_AllowedPlaceNumberNull.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DormitoryManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AllowedPlaceNumberNull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PlaceNumber",
+                table: "RoomPlaces",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PlaceNumber",
+                table: "RoomPlaces",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
