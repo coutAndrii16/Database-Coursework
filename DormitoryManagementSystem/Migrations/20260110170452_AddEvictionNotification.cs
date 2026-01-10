29734e21c80ac91abebc255ae9a12956591f75db
AndriiMaidanovych <ipz242_mav@student.ztu.edu.ua>
2026-01-10 19:29:14 +0200

    Added evictionNotifications

---
+++ DormitoryManagementSystem/Migrations/20260110170452_AddEvictionNotification.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DormitoryManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEvictionNotification : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EvictionNotifications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Reason = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EvictionDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BlockDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsRead = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EvictionNotifications", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EvictionNotifications_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EvictionNotifications_UserId",
+                table: "EvictionNotifications",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EvictionNotifications");
+        }
+    }
+}
